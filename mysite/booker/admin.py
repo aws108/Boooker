10687fdb2e1527d22cff1479e9cd59f6a8feb09b
aws108 <sergiolizangarcia@gmail.com>
2017-05-09 19:20:12 +0200

    dsd

---
+++ mysite/booker/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Genero
+
+admin.site.register(Genero)
+
+from .models import Libros
+
+admin.site.register(Libros)
+
+from .models import Capitulos
+
+admin.site.register(Capitulos)
