33fdbe6a068f7a1bae57da7ac84ea5df9371a084
aws108 <you@example.com>
2017-05-20 17:34:09 +0200

    actualizacion

---
+++ mysite/booker/views.py
@@ -0,0 +1,143 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from django.http import HttpResponse
+
+from booker.models import *
+# Create your views here.
+
+from django.contrib.auth import logout
+
+def logout_view(request):
+    logout(request)
+    return render( request, 'visualizadorlibros.html')
+
+class LibrosCreateView(CreateView):
+	model = Libros
+	# indiquem la plantilla personalitzada i els camps que han d'apareixer al formulari
+	# veureu que la plantilla es molt senzilla ja que fa tot el formulari amb {{form.as_p}}
+	template_name = "libros.html"
+	fields = ['nombre','genero','portada','contenido','sinopsis']
+
+class Libros2CreateView(CreateView):
+	model = Capitulos
+	# indiquem la plantilla personalitzada i els camps que han d'apareixer al formulari
+	# veureu que la plantilla es molt senzilla ja que fa tot el formulari amb {{form.as_p}}
+	template_name = "editor.html"
+	fields = ['nombre','libro','content']
+
+
+#VIEW PERSONALIZADA
+def visualizadorlibros(request,visualiza):
+	identificador = visualiza
+	
+	capitulos = Capitulos.objects.filter(libro=identificador)
+	return render( request, 'visualizadorlibros.html' , {"caps":capitulos} )
+
+def visualizadorcapitulos(request,visualiza2):
+	identificador2 = visualiza2
+
+	capitulos = Capitulos.objects.filter(id=identificador2)
+
+	return render( request, 'visualizadorcapitulos.html' , {"caps":capitulos} )
+
+		
+
+class LibrosListView(ListView):
+	model = Libros
+	template_name = "libroshecho.html"
+	# si no posem template_name agafara per defecte karaoke/item_list.html
+	def get_queryset(self):
+		# nomes posem els items que estiguin per cantar
+		return Libros.objects.all()
+		#.filter(fet=False)
+
+class LibrosAccionListView(ListView):
+	model = Libros
+	template_name = "libroshechoaccion.html"
+
+	def get_queryset(self):
+		# nomes posem els items que estiguin per cantar
+		return Libros.objects.all()
+		#.filter(fet=False)
+
+class LibrosAventuraListView(ListView):
+	model = Libros
+	template_name = "libroshechoaventura.html"
+	
+	def get_queryset(self):
+		# nomes posem els items que estiguin per cantar
+		return Libros.objects.all()
+		#.filter(fet=False)
+
+class LibrosCienciaFiccionListView(ListView):
+	model = Libros
+	template_name = "libroshechocienciaficcion.html"
+	
+	def get_queryset(self):
+		# nomes posem els items que estiguin per cantar
+		return Libros.objects.all()
+		#.filter(fet=False)
+
+class LibrosHumorListView(ListView):
+	model = Libros
+	template_name = "libroshechohumor.html"
+	
+	def get_queryset(self):
+		# nomes posem els items que estiguin per cantar
+		return Libros.objects.all()
+		#.filter(fet=False)
+
+class LibrosMisterioListView(ListView):
+	model = Libros
+	template_name = "libroshechomisterio.html"
+	
+	def get_queryset(self):
+		# nomes posem els items que estiguin per cantar
+		return Libros.objects.all()
+		#.filter(fet=False)
+
+class LibrosRomanceListView(ListView):
+	model = Libros
+	template_name = "libroshechoromance.html"
+	
+	def get_queryset(self):
+		# nomes posem els items que estiguin per cantar
+		return Libros.objects.all()
+		#.filter(fet=False)
+
+class LibrosPoesiaListView(ListView):
+	model = Libros
+	template_name = "libroshechopoesia.html"
+	
+	def get_queryset(self):
+		# nomes posem els items que estiguin per cantar
+		return Libros.objects.all()
+		#.filter(fet=False)
+
+class LibrosTerrorListView(ListView):
+	model = Libros
+	template_name = "libroshechoterror.html"
+	
+	def get_queryset(self):
+		# nomes posem els items que estiguin per cantar
+		return Libros.objects.all()
+		#.filter(fet=False)
+
+class LibrosDramaListView(ListView):
+	model = Libros
+	template_name = "libroshechodrama.html"
+	
+	def get_queryset(self):
+		# nomes posem els items que estiguin per cantar
+		return Libros.objects.all()
+		#.filter(fet=False)
+
+class LibrosOtrosListView(ListView):
+	model = Libros
+	template_name = "libroshechootros.html"
+	
+	def get_queryset(self):
+		# nomes posem els items que estiguin per cantar
+		return Libros.objects.all()
+		#.filter(fet=False)
---
+++ mysite/mysite/urls.py
@@ -0,0 +1,93 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+"""from django.conf.urls import url
+from django.contrib import admin
+from django.conf.urls import include
+
+urlpatterns = [
+	url(r'^booker/', include('booker.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^login/', include('social.apps.django_app.urls', namespace='social')),
+]"""
+
+import django
+
+
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.views.generic import TemplateView
+from booker import views
+from django.conf import settings
+from django.contrib.auth import views as auth_views
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls import *
+
+
+urlpatterns = [ 
+	url(r'^admin/', include(admin.site.urls)),
+	# Python Social Auth URLs
+	url('', include('social.apps.django_app.urls', namespace='social')),
+	url(r'^accounts/', include('registration.urls')),
+	# Home URL
+	url(r'^$', TemplateView.as_view(template_name="home.html"), name="home"),
+
+    #FORMULARIO
+	url(r'^libros/', views.LibrosCreateView.as_view(success_url='/hecho'), name="compres" ),
+
+    #CKEDITOR
+    url(r'^editor/', views.Libros2CreateView.as_view(success_url='/editor'), name="editor" ),
+    #url(r'^editor2/'
+
+    #TODOS LOS LIBROS
+	url(r'^hecho/', views.LibrosListView.as_view(), name="hecho"),
+
+    #GENEROS
+    url(r'^accion/', views.LibrosAccionListView.as_view(), name="accion"),
+    url(r'^aventura/', views.LibrosAventuraListView.as_view(), name="aventura"),
+    url(r'^cienciaficcion/', views.LibrosCienciaFiccionListView.as_view(), name="cienciaficcion"),
+    url(r'^humor/', views.LibrosHumorListView.as_view(), name="humor"),
+    url(r'^misterio/', views.LibrosMisterioListView.as_view(), name="misterio"),
+    url(r'^romance/', views.LibrosRomanceListView.as_view(), name="romance"),
+    url(r'^poesia/', views.LibrosPoesiaListView.as_view(), name="poesia"),
+    url(r'^terror/', views.LibrosTerrorListView.as_view(), name="terror"),
+    url(r'^drama/', views.LibrosDramaListView.as_view(), name="drama"),
+    url(r'^otros/', views.LibrosOtrosListView.as_view(), name="otros"),
+
+	# Logout URL
+	#url( r'^logout/$', auth_views.LogoutView.as_view() , {'next_page': '/'}, name="user-logout" ),
+
+    #Visualizador capitulos
+    url(r'^visualizador/(?P<visualiza>[0-9]{0,10})/', views.visualizadorlibros , name="visualizador"),
+    url(r'^visualizador2/(?P<visualiza2>[0-9]{0,10})/', views.visualizadorcapitulos , name="visualizador2"),
+
+
+	#url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+    #   'document_root': settings.STATIC_ROOT
+    #}),
+    url(r'^media/(?P<path>.*)$', django.views.static.serve, {
+        'document_root': settings.MEDIA_ROOT
+    }),
+    url(r'^djrichtextfield/', include('djrichtextfield.urls')),
+    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+
+    url(r'^ratings/', include('star_ratings.urls', namespace='ratings', app_name='ratings')),
+    url(r'^comments/', include('django_comments.urls')),
+
+]
+
+#urlpatterns += staticfiles_urlpatterns()
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
